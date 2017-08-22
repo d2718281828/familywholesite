3d96469937165fb78ee36459a9303d2368b6993b
Derek <Derek>
2017-08-22 21:41:21 +0100

    Added a few fields

--- public/wp-content/plugins/family-site/family-site.php
+++ public/wp-content/plugins/family-site/family-site.php
@@ -2,6 +2,7 @@
 namespace FamilySite;
 use CPTHelper\CptHelper;
 use CPTHelper\FieldHelper;
+use CPTHelper\DateHelper;
 /*
 Plugin Name: Family Site
 Plugin URI:
@@ -21,7 +22,21 @@ class FamilySite {
   protected function setupCPTs(){
 
     $z = (new CptHelper("person", "Person", "People", []))
-        ->addField(new FieldHelper("date_birth", "Date or Birth", "Date the person was born"))
+        ->addField(new DateHelper("date_birth", "Date of Birth", "Date the person was born, yyyy/mm/dd"))
+        ->addField(new CPTSelectHelper("place_birth", "Place of Birth", "Place the person was living in immediately after birth", ["posttype"=>"place"]))
+        ->addField(new DateHelper("date_death", "Date of Death", "Date the person was born, yyyy/mm/dd"))
+        ->addField(new CPTSelectHelper("place_death", "Place of Death", "Place the person was living when they died", ["posttype"=>"place"]))
+        ->addField(new CPTSelectHelper("father", "Father", "", ["posttype"=>"person"]))
+        ->addField(new CPTSelectHelper("mother", "Mother", "", ["posttype"=>"person"]))
+    ;
+    $z = (new CptHelper("event", "Event", "Events", []))
+        ->addField(new DateHelper("actual_date", "Actual date", "Date event started"))
+        ->addField(new FieldHelper("duration", "Duration", "The length of the event in days"))
+        ->addField(new CPTSelectHelper("event_place", "Place of the event", "Place where the event occurred", ["posttype"=>"place"]))
+    ;
+    $z = (new CptHelper("place", "Place", "Places", []))
+        ->addField(new FieldHelper("lat", "Latitude", "In degrees and decimals of a degree, + is North"))
+        ->addField(new FieldHelper("long", "Longitude", "Place the person was living when they died"))
     ;
   }
 
