eb0dfa62afc94288086b183d4fdb6ba3962a2c42
Derek <Derek>
2017-08-23 21:07:11 +0100

    More

---
+++ public/wp-content/plugins/family-site/class/FSCpt.php
@@ -0,0 +1,14 @@
+<?php
+namespace FamilySite;
+use CPTHelper\CptHelper;
+
+class FSCpt extends CptHelper {
+
+  protected function setup {
+    $this->prefix = "fs_";
+  }
+
+}
+
+
+ ?>
--- public/wp-content/plugins/family-site/family-site.php
+++ public/wp-content/plugins/family-site/family-site.php
@@ -1,9 +1,4 @@
 <?php
-namespace FamilySite;
-use CPTHelper\CptHelper;
-use CPTHelper\FieldHelper;
-use CPTHelper\DateHelper;
-use CPTHelper\CPTSelectHelper;
 /*
 Plugin Name: Family Site
 Plugin URI:
@@ -12,6 +7,14 @@ Author: Derek Storkey
 Version: 0.1
 Author URI:
 */
+namespace FamilySite;
+use CPTHelper\CptHelper;
+use CPTHelper\FieldHelper;
+use CPTHelper\DateHelper;
+use CPTHelper\CPTSelectHelper;
+
+require_once("class/FSCpt.php");
+
 class FamilySite {
 
   public function __construct(){
@@ -22,22 +25,26 @@ class FamilySite {
 
   protected function setupCPTs(){
 
-    $z = (new CptHelper("person", "Person", "People", []))
+    $z = (new FSCpt("person", "Person", "People", []))
         ->addField(new DateHelper("date_birth", "Date of Birth", "Date the person was born, yyyy/mm/dd"))
-        ->addField(new CPTSelectHelper("place_birth", "Place of Birth", "Place the person was living in immediately after birth", ["posttype"=>"place"]))
+        ->addField(new CPTSelectHelper("place_birth", "Place of Birth", "Place the person was living in immediately after birth", ["posttype"=>"fs_place"]))
         ->addField(new DateHelper("date_death", "Date of Death", "Date the person was born, yyyy/mm/dd"))
-        ->addField(new CPTSelectHelper("place_death", "Place of Death", "Place the person was living when they died", ["posttype"=>"place"]))
-        ->addField(new CPTSelectHelper("father", "Father", "", ["posttype"=>"person"]))
-        ->addField(new CPTSelectHelper("mother", "Mother", "", ["posttype"=>"person"]))
+        ->addField(new CPTSelectHelper("place_death", "Place of Death", "Place the person was living when they died", ["posttype"=>"fs_place"]))
+        ->addField(new CPTSelectHelper("father", "Father", "", ["posttype"=>"fs_person"]))
+        ->addField(new CPTSelectHelper("mother", "Mother", "", ["posttype"=>"fs_person"]))
+        ->addField(new SelectHelper("gender", "Gender", "", ["posttype"=>"fs_person"])
+            ->addOption("M","Male")
+            ->addOption("F","Female")
+        )
     ;
-    $z = (new CptHelper("event", "Event", "Events", []))
+    $z = (new FSCpt("event", "Event", "Events", []))
         ->addField(new DateHelper("actual_date", "Actual date", "Date event started"))
         ->addField(new FieldHelper("duration", "Duration", "The length of the event in days"))
-        ->addField(new CPTSelectHelper("event_place", "Place of the event", "Place where the event occurred", ["posttype"=>"place"]))
+        ->addField(new CPTSelectHelper("event_place", "Place of the event", "Place where the event occurred", ["posttype"=>"fs_place"]))
     ;
-    $z = (new CptHelper("place", "Place", "Places", []))
+    $z = (new FSCpt("place", "Place", "Places", []))
         ->addField(new FieldHelper("lat", "Latitude", "In degrees and decimals of a degree, + is North"))
-        ->addField(new FieldHelper("long", "Longitude", "Place the person was living when they died"))
+        ->addField(new FieldHelper("long", "Longitude", "In degrees, + is East, - is West."))
     ;
   }
 
