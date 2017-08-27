3147ed65e352e22d3522f71c90fd62e9d99cf806
Derek <Derek>
2017-08-27 17:31:31 +0100

    Saving term id

--- public/wp-content/plugins/family-site/class/FSCpt.php
+++ public/wp-content/plugins/family-site/class/FSCpt.php
@@ -18,20 +18,26 @@ class FSCpt extends CptHelper {
     if (WP_DEBUG) error_log("in FamilySite::on_save method");
     $name = $post->post_name;
     // do we have a matching tag?
-    $matchingtag = get_post_meta($post_id, "fs_matching_tag", true);
+    $matchingtag = get_post_meta($post_id, "fs_matching_tag_id", true);
     if ($matchingtag) return;
 
     // is there a tag with this postname?
     $term = get_term_by("slug", $name, $this->related_tax );
     if ($term) {
-	if (WP_DEBUG) error_log("eek, already have a tax term with name ".$name);
-	return;
+	    if (WP_DEBUG) error_log("eek, already have a tax term with name ".$name);
+	    return;
     }
 
-    wp_insert_term($post->post_title, $this->related_tax, [
+    $rc = wp_insert_term($post->post_title, $this->related_tax, [
         "description"=>"Term relating to post ".$post->post_title,
-        //"slug" => $name,
+        "slug" => $name,
       ] );
+    if (is_wp_error($rc)){
+      if (WP_DEBUG) error_log("Error inserting taxonomy term for ".$post->post_title.": ".$rc->get_error_message());
+    } else {
+      update_post_meta($post_id, "fs_matching_tag_id", $rc["term_id"]);
+      error_log("saving term id ".$rc["term_id"]);
+    }
   }
 
 }
