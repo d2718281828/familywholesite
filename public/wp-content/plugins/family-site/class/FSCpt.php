1a279ea2642ddfe5dc9d0f86e3353f906592027a
Derek <Derek>
2017-08-31 20:42:39 +0100

    Working on timeline

--- public/wp-content/plugins/family-site/class/FSCpt.php
+++ public/wp-content/plugins/family-site/class/FSCpt.php
@@ -34,7 +34,7 @@ class FSCpt extends CptHelper {
     if (is_wp_error($rc)){
       error_log("ERROR inserting taxonomy term for ".$post->post_title.", tax=".$this->related_tax.": ".$rc->get_error_message());
     } else {
-      update_post_meta($post_id, "fs_matching_tag_id", $rc["term_id"]);
+      update_post_meta($post_id, "fs_matching_tag_id", $rc["term_taxonomy_id"]);
       if (WP_DEBUG) error_log("saving term id ".$rc["term_id"]." for ".$post->post_title);
     }
   }
--- public/wp-content/plugins/family-site/class/TimeLine.php
+++ public/wp-content/plugins/family-site/class/TimeLine.php
@@ -9,7 +9,39 @@ class TimeLine {
     $this->focus = $focus;
   }
   public function html(){
-    return "timeline here";
+    global $wpdb;
+
+    $sql = $this->makeSQL();
+    $res = $wpdb->get_results($sql, ARRAY_A);
+
+    $m = "";
+    foreach($res as $post) $m.= " ".$post["ID"];
+    return $m;
+  }
+  protected function makeSQL(){
+    global $wpdb;
+
+    $select = ["P.ID"];
+    $from = [$wpdb->posts." P "];
+    $where = [];
+    $order = [];
+
+    if ($this->focus){
+      $tag = $this->focus->get("fs_matching_tag_id"); // this is the term_taxonomy_id
+      $from[] = $wpdb->term_relationships." TR ";
+      $where[] = "P.ID = TR.object_id";
+      $where[] = "TR.term_taxonomy_id = ".((int)$tag);
+    }
+    // get the date
+    $from[] = $wpdb->postmeta." D ";
+    $select[] = "D.meta_value as actual_date";
+    $where[] = "P.ID = D.post_id";
+    $where[] = "D.meta_key = 'actual_date'";
+    $order[] = "actual_date DESC";
+
+    $s = "SELECT ".implode(",",$select)." FROM ".implode(",",$from);
+    $s.= " WHERE (".implode(") and (", $where). ") ORDER BY ".implode(",",$order);
+    return $s;
   }
 }
  ?>
