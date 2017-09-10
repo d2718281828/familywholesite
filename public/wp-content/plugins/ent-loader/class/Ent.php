5bffc53d6b9a8d9714370a4d9315a86c0563181f
Derek <Derek>
2017-09-10 16:25:22 +0100

    and file

---
+++ public/wp-content/plugins/ent-loader/class/Ent.php
@@ -0,0 +1,30 @@
+<?php
+namespace EntLoader;
+
+class Ent  {
+	
+	protected $key;
+	protected $type = "";
+	protected $sourcedir;
+	protected $reldir;
+	protected $size = 0;
+	
+	public function __construct($filename, $reldir, $fulldir){
+		$this->reldir = $reldir;
+		$this->sourcedir = $fulldir;
+		$this->key = str_replace(".txt","",strtolower($filename));
+		$this->getit();
+	}
+	public function key(){
+		return $this->key;
+	}
+	protected function getit(){
+		$content = file_get_contents($this->sourcedir);
+		$this->size = strlen($content);
+	}
+	public function show(){
+		return $this->key.'-'.$this->size;
+	}
+
+
+}
