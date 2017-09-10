f4b3561055b74763747c23d4432388c59fa6acbf
Derek <derek@storkey.co.uk>
2017-09-10 11:38:37 -0400

    added first line to show

--- public/wp-content/plugins/ent-loader/class/Ent.php
+++ public/wp-content/plugins/ent-loader/class/Ent.php
@@ -8,12 +8,14 @@ class Ent  {
 	protected $sourcedir;
 	protected $reldir;
 	protected $size = 0;
+	protected $firstline;
+	protected $numlines = 0;
 	
 	public function __construct($filename, $reldir, $fulldir){
 		$this->reldir = $reldir;
 		$this->sourcedir = $fulldir;
 		$this->key = str_replace(".txt","",strtolower($filename));
-		$this->getit();
+		$this->getit($filename);
 	}
 	public function key(){
 		return $this->key;
@@ -21,9 +23,12 @@ class Ent  {
 	protected function getit($fname){
 		$content = file_get_contents($this->sourcedir.$fname);
 		$this->size = strlen($content);
+		$lines = explode("\n",$content);
+		$this->firstline = $lines[0];
+		$this->numlines = count($lines);
 	}
 	public function show(){
-		return $this->key.'-'.$this->size.'('.$this->sourcedir.')';
+		return $this->key.'-'.$this->size.'-'.$this->numlines.'('.htmlentities(trim($this->firstline)).')';
 	}
 
 
