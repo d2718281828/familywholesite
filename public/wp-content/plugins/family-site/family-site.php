8216b7deb6f22755648c5ac3ebd400081987267d
Derek <Derek>
2018-02-17 11:12:17 +0000

    doc

--- public/wp-content/plugins/family-site/family-site.php
+++ public/wp-content/plugins/family-site/family-site.php
@@ -8,8 +8,8 @@ Version: 0.1
 Author URI:
 */
 /* TODOs
-private data type
-images on front page are full size. need 524 x 314, for any width over 920
+bug for john story talyor http://dev.storkey.uk/fs_person/john-storkeytaylor/
+private data attribute on import
 need to store the type of media for interest
 timeline entries arent being deleted when post is deleted
 change 'leave a reply to 'make a comment
